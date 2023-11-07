206fd226d5755f6b57f7a9f2a74c3275836622ff
Raian Melon <rainamelon11@gmail.com>
2023-11-07 20:20:08 +0100

    Installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-
+import './index.css';
+import Employee from "./components/Employee";
+import {useState} from "react";
 function App() {
+  const [role, setRole] = useState('dev')
+  const showEmplyees = true
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello!
-        </p>
-        <a          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Raact
-        </a>
-      </header>
+      {showEmplyees ?
+          <>
+            <input type='text' onChange={(e) => {console.log(e.target.value); setRole(e.target.value)}} />
+            <Employee name="Caleb" role="Intern"/>
+            <Employee name="Abby" role={role}/>
+            <Employee name="John"/>
+          </>
+          :
+          <p>You cannot see the employees</p>
+      }
+
     </div>
   );
 }
